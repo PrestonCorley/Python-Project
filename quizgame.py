58c8cb48fe14a132375a43a7f93d52cf9bd04cb1
Ranch <31449608+Ranch@users.noreply.github.com>
2018-02-23 10:43:50 -0800

    Add files via upload
    part 1

---
+++ quizgame.py
@@ -0,0 +1,47 @@
+import random
+def q1():
+    question = raw_input("What type of animal is Bambi?")
+        if question == "deer":
+            score = score + 1
+        else:
+            print("Guess again")
+def q2():
+    question = raw_input("Which country features a maple leaf on its flag?")
+    if question == "Canada":
+        score = score + 1
+    else:
+            print("Guess again")
+def q3():
+    question = raw_input("Name a US state beginning with K?")
+    if question == "Kansas", "Kentucky":
+        score = score + 1
+    else:
+            print("Guess again")
+def q4():
+    question = raw_input("What is the official language of Brazil?")
+    if question == "Portuguese":
+        score = score + 1
+    else:
+            print("Guess again")
+def q5():
+     question = raw_input("What is the official language of Brazil?")
+    if question == "Portuguese":
+        score = score + 1
+    else:
+            print("Guess again")
+def q6():
+    question = raw_input("What superhero has been played by Michael Keaton, Val Kilmer, George Clooney and Christian Bale?")
+    if question == "Batman":
+        score = score + 1
+    else:
+            print("Guess again")
+def q7():
+     question = raw_input("Which month is Black History Month in USA?")
+    if question == "February":
+        score = score + 1
+    else:
+            print("Guess again")
+def quiz():
+    score = 0
+    return score
+    
